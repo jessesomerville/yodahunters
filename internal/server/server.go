77d1c6bdaa7b05edb284a07b96a0f00df5d81fe9
jessesomerville <jssomerville2@gmail.com>
2025-08-02 10:14:26 -0700

    feat(internal/log): add logging helper package

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,11 +3,12 @@ package server
 
 import (
 	"bytes"
+	"context"
 	"io"
-	"log"
 	"net/http"
 
 	"github.com/google/safehtml/template"
+	"github.com/jessesomerville/yodahunters/internal/log"
 	"github.com/jessesomerville/yodahunters/internal/templates"
 )
 
@@ -23,7 +24,7 @@ type Server struct {
 }
 
 // Run starts the server and returns an error upon exit.
-func Run(cfg Config) error {
+func Run(ctx context.Context, cfg Config) error {
 	renderer, err := templates.New(cfg.TemplateFS)
 	if err != nil {
 		return err
@@ -34,25 +35,25 @@ func Run(cfg Config) error {
 
 	mux := http.NewServeMux()
 	mux.Handle("/", s.handleHome())
-	log.Printf("Serving site at %q\n", cfg.Address)
+	log.Infof(ctx, "Serving site at %q\n", cfg.Address)
 	return http.ListenAndServe(cfg.Address, mux)
 }
 
-func (s *Server) serveHTML(w http.ResponseWriter, tmpl string, data any) {
+func (s *Server) serveHTML(ctx context.Context, w http.ResponseWriter, tmpl string, data any) {
 	buf, err := s.renderer.Render(tmpl, data)
 	if err != nil {
-		log.Printf("serveHTML(w, %q, %+v): %v", tmpl, data, err)
+		log.Errorf(ctx, "serveHTML(w, %q, data): %v", tmpl, err)
 		// TODO - Replace this with a proper error page response.
 		http.Error(w, "failed to render page", http.StatusInternalServerError)
 		return
 	}
 	if _, err := io.Copy(w, bytes.NewReader(buf)); err != nil {
-		log.Printf("Server.serveHTML(w, %q, data): failed to write to http.ResponseWriter: %v", tmpl, err)
+		log.Errorf(ctx, "serveHTML(w, %q, data): failed to write to http.ResponseWriter: %v", tmpl, err)
 	}
 }
 
 func (s *Server) handleHome() http.HandlerFunc {
-	return func(w http.ResponseWriter, _ *http.Request) {
-		s.serveHTML(w, "home", nil)
+	return func(w http.ResponseWriter, r *http.Request) {
+		s.serveHTML(r.Context(), w, "home", nil)
 	}
 }
