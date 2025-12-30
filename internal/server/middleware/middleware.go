ba1093db96988edd4fd5168ac10ee7e77a0f8220
btpost <bpost.brad@gmail.com>
2025-12-30 16:00:46 -0500

    created admin middleware

--- internal/server/middleware/middleware.go
+++ internal/server/middleware/middleware.go
@@ -10,11 +10,16 @@ import (
 
 type ctxKey string
 
+// CtxSecretKey is used to set and retrieve the JWT signing secret
+const CtxSecretKey ctxKey = "jwt_secret"
+
 // CtxUserKey is used to set and retrieve the user ID from a request context.
 const CtxUserKey ctxKey = "userID"
 
 // CtxPageKey is used to set and retrieve page data
 const CtxPageKey ctxKey = "page"
+
+const CtxAdminKey ctxKey = "isAdmin"
 
 // AuthorizationHandler verifies that a request has a valid access token in the
 // cookie, retrieves the user_id set in the access token, and adds the user_id
@@ -27,7 +32,14 @@ func AuthorizationHandler(next http.HandlerFunc, secret []byte) http.HandlerFunc
 			log.Errorf(r.Context(), "Authorization Failed!")
 			return
 		}
+		isAdmin, err := IsAdmin(r, secret)
+		if err != nil {
+			http.Redirect(w, r, "/", http.StatusFound)
+			log.Errorf(r.Context(), "Admin authorization Failed!")
+			return
+		}
 		ctx := context.WithValue(r.Context(), CtxUserKey, userID)
+		ctx = context.WithValue(ctx, CtxAdminKey, isAdmin)
 		next.ServeHTTP(w, r.WithContext(ctx))
 	}
 }
@@ -43,8 +55,24 @@ func PageHandler(next http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+func AdminHandler(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		isAdmin := r.Context().Value(CtxAdminKey)
+		if isAdmin != true {
+			log.Errorf(r.Context(), "Admin authorization Failed!")
+			http.Redirect(w, r, "/", http.StatusFound)
+			return
+		}
+		next.ServeHTTP(w, r)
+	}
+}
+
 // Chain links all the middleware handlers together to make it convenient to call them all
 // in a row.
 func Chain(f func(w http.ResponseWriter, r *http.Request) error, jwtSecret []byte) http.HandlerFunc {
 	return AuthorizationHandler(PageHandler(ErrorHandler(f)), jwtSecret)
+}
+
+func AdminChain(f func(w http.ResponseWriter, r *http.Request) error, jwtSecret []byte) http.HandlerFunc {
+	return AuthorizationHandler(PageHandler(AdminHandler(ErrorHandler(f))), jwtSecret)
 }
