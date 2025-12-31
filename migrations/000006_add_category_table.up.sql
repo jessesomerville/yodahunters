3526f695538669e9e75a801dea89dfc85acf5db4
btpost <bpost.brad@gmail.com>
2025-12-31 12:52:19 -0500

    implement categories in API

--- migrations/000006_add_category_table.up.sql
+++ migrations/000006_add_category_table.up.sql
@@ -4,12 +4,14 @@ BEGIN;
 
 CREATE TABLE categories (
 	category_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	title VARCHAR(255),
-	description TEXT
+	title VARCHAR(255) NOT NULL,
+	description TEXT NOT NULL,
+	author_id INT NOT NULL REFERENCES users(id),
+  	created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO categories (title, description) 
-    VALUES ('General', 'Just chatting...');
+INSERT INTO categories (title, description, author_id) 
+    VALUES ('General', 'Just chatting...', 1);
 
 ALTER TABLE threads ADD COLUMN category_id INT REFERENCES categories(category_id) DEFAULT 1;
 
